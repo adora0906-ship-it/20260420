3d245e5a203eeb48811617f32682ca348f762228
adora0906 <adora0906@gmail.com>
2026-04-20 10:32:03 +0800

    修改左右顛倒的問題

--- sketch.js
+++ sketch.js
@@ -15,8 +15,14 @@ function draw() {
   let vWidth = width * 0.6;
   let vHeight = height * 0.6;
 
-  // Draw the video centered on the canvas
+  // 修正攝影機左右顛倒（鏡像）問題
+  push();
+  // 將座標系統平移至畫布右側，並反轉 X 軸
+  translate(width, 0);
+  scale(-1, 1);
+  // 在翻轉後的座標系統中繪製影像，影像會呈現鏡像效果
   image(capture, (width - vWidth) / 2, (height - vHeight) / 2, vWidth, vHeight);
+  pop();
 }
 
 function windowResized() {
